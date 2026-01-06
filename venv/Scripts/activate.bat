ec0e9c1d2f7e3b808636706be84cc9a3a854b48b
Yovinne <yovinne.shadora@students.jkuat.ac.ke>
2026-01-06 15:23:42 +0300

    Initial commit with .gitignore and README

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\yovin\OneDrive\Documents\Emotion app\venv
+set VIRTUAL_ENV=C:\Users\yovin\OneDrive\Desktop\Yovinne\Yovinne Doc\Data science\Emotion app\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
